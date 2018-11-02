ed8a8812a1f7b37e2200d9f173f24d5ffd1761a8
Annabel <Annabel@localhost>
2018-11-02 02:16:31 -0700

    ADDED USER INPUT LETS GO

--- SplitString/src/Split.java
+++ SplitString/src/Split.java
@@ -3,20 +3,44 @@
  * @version November 2 2018
  */
 
-import java.util.Arrays;
+import java.util.*;
 
 public class Split {
 
 	public static void main(String[] args) {
+		boolean done = false;
 		
-	System.out.println(Arrays.toString(partOne("applesbreadpineapplesbreadlettucetomatobaconmayohambreadcheesebread")));
-	System.out.println(Arrays.toString(partTwo("apples pineapples bread lettuce bread tomato bacon bread mayo bread ham bread cheese")));
+		while (!done) {
+			Scanner userInput = new Scanner(System.in);	
+			userInput.useDelimiter("\\n");
+			
+			System.out.print("Are you splitting sandwiches by bread (part 1) or spaces (part 2)? Or, type 'done' when done. ");		
+			String input = userInput.next();
+			String splitBy = input.toLowerCase();
+			
+			if (splitBy.equals("done")) {
+				done = true;
+			} else {
+				System.out.print("Insert the sandwich you would like to split. ");			
+				String sandwich = userInput.next();
+				
+				if (splitBy.equals("bread")) {
+					System.out.println(Arrays.toString(partOne(sandwich)));
+					
+				} else if (splitBy.equals("spaces")) {
+					System.out.println(Arrays.toString(partTwo(sandwich)));
+				
+				} else {
+					System.out.println("\nPlease type exactly 'bread', 'spaces', or 'done'. ");
+				}
+				
+			}
+		}
 	}
- 
 	
 	public static String[] partOne(String sandwich) {
 		String[] splitSand = sandwich.split("bread"); //generate the array
-		return(Arrays.copyOfRange(splitSand, 1, splitSand.length)); //return the parts between first and last bread
+		return(Arrays.copyOfRange(splitSand, 1, (splitSand.length-1))); //return the parts between first and last bread
 	}
 	
 	public static String[] partTwo(String sandwich) {
